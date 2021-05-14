12e0e2dfd612ca017879785bb4b91aa0678f3f35
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-05-14 16:31:45 +0200

    Fixed wrong .gitignore Implemented basic setup for testing including mocks for oven interfaces.

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -3,12 +3,26 @@ package edu.iis.mto.oven;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
+
+    @Mock
+    private HeatingModule heatingModule;
+    @Mock
+    private Fan fan;
+
+    private Oven oven;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        this.oven=new Oven(heatingModule,fan);
+    }
 
     @Test
     void itCompiles() {

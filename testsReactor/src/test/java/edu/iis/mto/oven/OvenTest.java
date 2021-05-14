9d560ee64b027b977538104257920e4a4a3ce20c
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-05-14 17:28:20 +0200

    Implemented first behavioural test where there's one program stage with heating type set to heat.

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -1,13 +1,17 @@
 package edu.iis.mto.oven;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.inOrder;
 
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
@@ -18,10 +22,17 @@ class OvenTest {
     private Fan fan;
 
     private Oven oven;
+    private BakingProgram bakingProgram;
+    private ArrayList<ProgramStage> programStages= new ArrayList<>();
+    private HeatingSettings initHeatingSettings;
 
     @BeforeEach
     void setUp() throws Exception{
         this.oven=new Oven(heatingModule,fan);
+        initHeatingSettings = HeatingSettings.builder()
+                .withTargetTemp(100)
+                .withTimeInMinutes(0)
+                .build();
     }
 
     @Test
@@ -29,4 +40,20 @@ class OvenTest {
         MatcherAssert.assertThat(true, equalTo(true));
     }
 
+    @Test
+    void oneProgramStageHeatingTypeSetToHeat() throws HeatingException {
+        programStages.add(ProgramStage.builder().withStageTime(100).withTargetTemp(200).withHeat(HeatType.HEATER).build());
+        bakingProgram= BakingProgram.builder().withInitialTemp(100).withStages(programStages).build();
+        HeatingSettings properHeatingSettings= HeatingSettings.builder()
+                .withTargetTemp(200)
+                .withTimeInMinutes(100)
+                .build();
+
+
+        oven.start(bakingProgram);
+        InOrder callOrder= inOrder(heatingModule,fan);
+        callOrder.verify(heatingModule).heater(initHeatingSettings);
+        callOrder.verify(fan).isOn();
+        callOrder.verify(heatingModule).heater(properHeatingSettings);
+    }
 }
